1b11733b51e4158919c44a290c02f7a2d7244fc1
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-16 12:44:08 +0530

    Communicating with web server

---
+++ crawler.py
@@ -0,0 +1,10 @@
+import requests
+import requests
+
+url = "mail.google.com"
+
+try:
+    get_response = requests.get("http://" + url)
+    print(get_response)
+except requests.exceptions.ConnectionError:
+    pass    
