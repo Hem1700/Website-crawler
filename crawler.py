cef5ab59781e86e05c866edc828545382ca93823
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-17 09:39:23 +0530

    Checking subdomains using a domainlist

--- crawler.py
+++ crawler.py
@@ -1,9 +1,19 @@
 import requests
 
-url = "mail.google.com"
 
-try:
-    get_response = requests.get("http://" + url)
-    print(get_response)
-except requests.exceptions.ConnectionError:
-    pass    
+def request(url):
+    try:    
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass    
+
+target_url = "google.com"
+with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\subdomains.txt", "r") as word_list:
+    for line in word_list:
+        word = line.strip()
+        test_url = word + "." + target_url
+        response = request(target_url)
+        if response:
+            print("[+] Discovered a subdomain --> " + response)
+
+  
