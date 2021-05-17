bfa64c35c4921be65f06138a1071d6ba3683d161
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-17 10:32:45 +0530

    Checking directories using word list

--- crawler.py
+++ crawler.py
@@ -7,7 +7,9 @@ def request(url):
     except requests.exceptions.ConnectionError:
         pass    
 
-target_url = "google.com"
+target_url = "10.0.2.4/mutillidae/"
+
+# To check the subdomains 
 with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\subdomains.txt", "r") as word_list:
     for line in word_list:
         word = line.strip()
@@ -16,4 +18,12 @@ with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\subdomains.txt", "r") as 
         if response:
             print("[+] Discovered a subdomain --> " + test_url)
 
-  
+
+# To check directories
+with open("C:\\Users\dharm\\PycharmProjects\\crawler\\directories.txt", "r") as directory_list:
+    for line in directory_list:
+        directory = line.strip()
+        direct_url =  target_url + "/" + directory
+        response = request(direct_url)
+        if response:
+            print("[+] Discovered URL --> " + direct_url)
