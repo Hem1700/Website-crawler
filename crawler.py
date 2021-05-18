d175cf389283345fb57ebdad762f83939bde0df4
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-18 16:01:16 +0530

    Filtering results

--- crawler.py
+++ crawler.py
@@ -1,34 +1,42 @@
 import requests
 import re
+import urllib.parse
 
-def request(url):
-    try:    
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
-        pass    
 
 target_url = "10.0.2.4/mutillidae/"
 
+def extract_link(url):
 # To get all the links on a webpage 
-repsonse = request(target_url)
-href_links = re.findall('(?:href=")(.*?)"', str(repsonse.content))
-print(href_links)
+    repsonse = requests.get("http://" + url)
+    return re.findall('(?:href=")(.*?)"', str(repsonse.content))
 
+href_link = extract_link(target_url)
+for link in href_link:
+    link  = urllib.parse.urljoin(target_url, link)
+    if target_url in link:
+        print(href_link)
+
+
+def get_subdomains(url):
 # To check the subdomains 
-with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\subdomains.txt", "r") as word_list:
-    for line in word_list:
-        word = line.strip()
-        test_url = word + "." + target_url
-        response = request(target_url)
-        if response:
-            print("[+] Discovered a subdomain --> " + test_url)
+    with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\subdomains.txt", "r") as word_list:
+        for line in word_list:
+            word = line.strip()
+            test_url = word + "." + target_url
+            response = requests.get(target_url)
+            if response:
+                print("[+] Discovered a subdomain --> " + test_url)
 
 
+def get_directories(url):
 # To check directories
-with open("C:\\Users\dharm\\PycharmProjects\\crawler\\directories.txt", "r") as directory_list:
-    for line in directory_list:
-        directory = line.strip()
-        direct_url =  target_url + "/" + directory
-        response = request(direct_url)
-        if response:
-            print("[+] Discovered URL --> " + direct_url)
+    with open("C:\\Users\dharm\\PycharmProjects\\crawler\\directories.txt", "r") as directory_list:
+        for line in directory_list:
+            directory = line.strip()
+            direct_url =  target_url + "/" + directory
+            response = requests.get(direct_url)
+            if response:
+                print("[+] Discovered URL --> " + direct_url)
+
+
+
